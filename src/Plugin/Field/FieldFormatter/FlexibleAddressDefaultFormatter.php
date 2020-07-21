0f24a44f4891eba27e578c459ca940e377f62536
Tiago Simões <tiago.simoes@uni-foundation.eu>
2020-07-21 18:14:55 +0000

    Added Flexible address field type

---
+++ src/Plugin/Field/FieldFormatter/FlexibleAddressDefaultFormatter.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\ewp_contact\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'ewp_flexible_address_default' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "ewp_flexible_address_default",
+ *   label = @Translation("Default"),
+ *   field_types = {
+ *     "ewp_flexible_address"
+ *   }
+ * )
+ */
+class FlexibleAddressDefaultFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      // Implement default settings.
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return [
+      // Implement settings form.
+    ] + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = ['#markup' => $this->viewValue($item)];
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    $output = [];
+    $output .= $item['recipient_name'] . '<br />';
+    $output .= $item['country'] . '<br />';
+
+    return $output;
+  }
+
+}
